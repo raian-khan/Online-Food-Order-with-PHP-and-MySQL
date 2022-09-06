d8ec874cf80d0bc641004f569af86fcb865fdb1b
Md. Nurul Yousuf Khan <65726004+Aninda-1281@users.noreply.github.com>
2022-09-06 12:11:08 +0600

    Adi update

---
+++ onlinefood-order/partials-front/footer.php
@@ -0,0 +1,11 @@
+<section class="social">
+    </section>
+
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved.</p>
+        </div>
+    </section>
+
+</body>
+</html>
